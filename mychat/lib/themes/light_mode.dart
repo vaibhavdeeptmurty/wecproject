754c93377f1bb7af0a23cf0e8f0d9158cbee86f7
vaibhavdeeptmurty <vaibhavdeeptmurty@gmail.com>
2025-10-19 18:19:02 +0530

    creating base chat screen

--- mychat/lib/themes/light_mode.dart
+++ mychat/lib/themes/light_mode.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
+
 // light mode theme
 ThemeData lightMode = ThemeData(
-  // app bar theme
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    elevation: 1,
-    iconTheme:  IconThemeData(color:Colors.grey.shade400),
-    backgroundColor: Colors.grey.shade700,
-    titleTextStyle: TextStyle(
-      color: Colors.grey.shade400,
-      fontSize: 20,
-      fontWeight: FontWeight.w400
-      ,
+    // app bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      elevation: 1,
+      iconTheme: IconThemeData(color: Colors.grey.shade400),
+      backgroundColor: Colors.grey.shade700,
+      titleTextStyle: TextStyle(
+        color: Colors.grey.shade400,
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+      ),
     ),
-    ),
-  //   main color scheme
-  colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade300,
-    primary: Colors.grey.shade600,
-    secondary: Colors.grey.shade400,
-    tertiary: Colors.white,
-    inversePrimary: Colors.grey.shade900,
-
-  )
-);
+    //   main color scheme
+    colorScheme: ColorScheme.light(
+      surface: Colors.grey.shade300,
+      primary: Colors.grey.shade600,
+      secondary: Colors.grey,
+      tertiary: Colors.white,
+      inversePrimary: Colors.grey.shade900,
+    ));
--- mychat/lib/widgets/chat_user_card.dart
+++ mychat/lib/widgets/chat_user_card.dart
@@ -1,9 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:mychat/models/chat_user.dart';
 import 'package:cached_network_image/cached_network_image.dart';
-import '../main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mychat/models/chat_user.dart';
 
+import '../main.dart';
+import '../screens/chat_screen.dart';
 
 class ChatUserCard extends StatefulWidget {
   final ChatUser user;
@@ -14,34 +15,47 @@ class ChatUserCard extends StatefulWidget {
 }
 
 class _ChatUserCardState extends State<ChatUserCard> {
-
   @override
   Widget build(BuildContext context) {
-    return  Card(
-      child: InkWell(child: ListTile(
-        // profile pic
-        leading: ClipOval(
-          child: CachedNetworkImage(
-            width: mq.height*0.05,
-            height:mq.height*0.05,
-            imageUrl: widget.user.image,
-            errorWidget: (context, url, error) => const Icon(CupertinoIcons.person),
-          ),
-        ),
-        // user name
-        title: Text(widget.user.name),
-        // last message
-        subtitle: Text(widget.user.about, maxLines: 1,),
-        // last message time
-        trailing: Container(height: 15,width: 15,decoration: BoxDecoration(
-                              color: Colors.green.shade400,
-                              borderRadius: BorderRadius.circular(10),
-                              )
-                  )
-        // Text('12:00 PM',style: TextStyle(fontSize: 12,color: Theme.of(context).colorScheme.primary),),
-      )
-      ),
+    return Card(
+      child: InkWell(
+          onTap: () {
+            // FOR NAVIGATING TO CHAT SCREEN
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (_) => ChatScreen(
+                          user: widget.user,
+                        )));
+          },
+          child: ListTile(
+              // profile pic
+              leading: ClipOval(
+                child: CachedNetworkImage(
+                  width: mq.height * 0.05,
+                  height: mq.height * 0.05,
+                  imageUrl: widget.user.image,
+                  errorWidget: (context, url, error) =>
+                      const Icon(CupertinoIcons.person),
+                ),
+              ),
+              // user name
+              title: Text(widget.user.name),
+              // last message
+              subtitle: Text(
+                widget.user.about,
+                maxLines: 1,
+              ),
+              // last message time
+              trailing: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: BoxDecoration(
+                    color: Colors.green.shade400,
+                    borderRadius: BorderRadius.circular(10),
+                  ))
+              // Text('12:00 PM',style: TextStyle(fontSize: 12,color: Theme.of(context).colorScheme.primary),),
+              )),
     );
   }
 }
-
